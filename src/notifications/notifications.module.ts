47186c7693b0bb45fe388bffc5335b9491403f59
codesailor4 <codesailor4@gmail.com>
2026-07-27 11:29:12 +0100

    Feat/websocket (#999)
    * implemnted the websocket
    
    * implemnted the websocket
    
    * implemnted the websocket
    
    * implemnted the fix bootstrap
    
    * implemnted the search functionality

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -15,6 +15,7 @@ import {
 import { PrismaModule } from '../database/prisma.module';
 import { EmailModule } from '../email/email.module';
 import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [PrismaModule, EmailModule, UsersModule, ConfigModule],
